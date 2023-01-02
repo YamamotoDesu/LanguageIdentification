1be067838a673e2f85cf66d34314fac4e4671822
YamamotoDesu <mynameisyamamotodesu@gmail.com>
2023-01-02 14:13:32 +0900

    Named Entity Recognition

--- LanguageAndText.playground/Contents.swift
+++ LanguageAndText.playground/Contents.swift
@@ -24,8 +24,38 @@ private func predictLanguage(text: String) -> String? {
 
 
 
-["Hello World","こんにちは", "Holla Comos Estas","5%%5","ہیلو آپ کیسے ہیں؟","Hello Holla"].forEach {
-    if let prediction = predictLanguage(text: $0) {
-        print(prediction)
+//["Hello World","こんにちは", "Holla Comos Estas","5%%5","ہیلو آپ کیسے ہیں؟","Hello Holla"].forEach {
+//    if let prediction = predictLanguage(text: $0) {
+//        print(prediction)
+//    }
+//}
+
+private func printNamedEntities(text: String) {
+    
+    let tagger = NSLinguisticTagger(tagSchemes: [.nameType], options: 0)
+    tagger.string = text
+    
+    let range = NSRange(location: 0, length: text.count)
+    
+    let options: NSLinguisticTagger.Options = [.omitPunctuation, .omitWhitespace, .joinNames]
+    
+    let tags: [NSLinguisticTag] = [.personalName, .placeName, .organizationName]
+    
+    tagger.enumerateTags(in: range, unit: .word, scheme: .nameType, options: options) { tag, tokenRange, stop in
+        
+        if let tag = tag, tags.contains(tag) {
+            let name = (text as NSString).substring(with: tokenRange)
+            print("\(name) is a \(tag.rawValue)")
+        }
+        
     }
+    
 }
+
+
+["My name is John and I love to visit Australia", "My name is Mary and I work for Google","Yesterday, I went to Costa Rica and also met with James who works at Exxon", "Holla Comos Estas", "Yamamoto works at AMBL"].forEach { text in
+        
+    printNamedEntities(text: text)
+    
+}
+
